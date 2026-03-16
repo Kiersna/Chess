635f0f6ffbe36538a77b98142e5673372c72c4e3
kiersna <kiersnowskid@gmail.com>
2026-03-16 14:10:42 +0100

    klasy

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,29 @@
+﻿namespace ChessLogic
+{
+    public class Direction
+    {
+        public readonly static Direction North = new Direction(-1, 0);
+        public readonly static Direction South = new Direction(1, 0);
+        public readonly static Direction East = new Direction(0, 1);
+        public readonly static Direction West = new Direction(0, -1);
+        public readonly static Direction NorthWest = North + West;
+        public readonly static Direction SouthWest = South + West;
+        public readonly static Direction NorthEast = North + East;
+        public readonly static Direction SouthEast = South + East;
+        public int rowDelta { get; }
+        public int colDelta { get; }
+        public Direction(int rowDelta, int colDelta)
+        {
+            this.rowDelta = rowDelta;
+            this.colDelta = colDelta;
+        }
+        public static Direction operator +(Direction dir1, Direction dir2)
+        {
+            return new Direction(dir1.rowDelta + dir2.rowDelta, dir1.colDelta + dir2.colDelta);
+        }
+        public static Direction operator *(int scalar, Direction dir2)
+        {
+            return new Direction(scalar * dir2.rowDelta, scalar * dir2.colDelta);
+        }
+    }
+}
---
+++ ChessLogic/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+    public static class PlayerExtansions
+    {
+        public static Player Opponent(Player player)
+        {
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None,
+            };
+            //to samo dzialanie na dole 
+            //switch (player)
+            //{
+            //    case Player.White:
+            //        return Player.Black;
+            //    case Player.Black:
+            //        return Player.White;
+            //    default:
+            //        return Player.None;
+            //}
+        }
+    }
+}
